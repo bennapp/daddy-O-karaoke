5b5cc28a2f121320f4e155e98748eb76d46331e8
Ben Nappier <bennappier@gmail.com>
2018-12-08 12:56:02 -0800

    list up

--- generate_song_list.rb
+++ generate_song_list.rb
@@ -1,21 +1,25 @@
 lib = {}
 
-Dir.foreach('D:\downloaded\karaoke-collection-backups') do |item|
+Dir.foreach('D:\downloaded\karaoke-collection') do |item|
+  next if item.nil?
   next if item == '.' or item == '..'
   next if item[-4..-1] == '.cdg'
   artist, title_with_lib_and_file_name = item.split(' -')
-  
+ 
+  next if title_with_lib_and_file_name.nil?
+
   title_with_lib = title_with_lib_and_file_name.split('.mp3').first
   title = title_with_lib.gsub(/ \[[a-zA-Z]+ Karaoke\]/, '')
   title = title[1..-1]
 
-  lib[artist] ||= []
+  lib[artist] ||= { 'name' => artist, 'songs' => [] }
+  song = { 'title' => title }
 
-  lib[artist] << title unless lib[artist].any? { |t| t.casecmp(title) == 0 }
+  lib[artist]['songs'] << song unless lib[artist]['songs'].any? { |s| s['title'].casecmp(title) == 0 }
 end
 
 require 'yaml'
 
 File.open("_data/lib.yml","w") do |file|
-   file.write lib.to_yaml
+  file.write lib.values.to_yaml
 end 
